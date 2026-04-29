dbf3cba59321bfd94db28a44db0b01aa11697665
otattemita <otakun@outlook.com>
2026-04-29 16:50:58 -0700

    chore: configure for custom domain gelblaster.life

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gel_blaster_compare/',
+  base: '/',
   server: {
     host: true
   }
